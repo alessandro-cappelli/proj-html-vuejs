23cbaac5f5a33d0034169b110b9482f9aeffb2f3
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2022-02-08 15:41:46 +0100

    social-call

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,26 @@
 <template>
-    <section>
+    <section class="ms_container_info">
+        <div class="container">
+            <div class="row">
+                <div class="col d-flex">
+                    <div class="col d-flex">
+                        <div class="ms_icon_call"><i class="fas fa-phone-alt"></i></div>
+                        <p>Call us for a Free Quote: 1.800.555.6789</p>
+                    </div>
 
+                <div class="col d-flex justify-content-end">
+                    <ul>
+                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                    </ul>
+                </div>
+                </div>
+            </div>
+        </div>
     </section>
+    
 </template>
 
 <script>
@@ -11,5 +30,26 @@ export default {
 </script>
 
 <style>
+.row{
+    height: 50px;
+    align-items: center;
+}
+.ms_container_info{
+    background-color: #6abf17;
+    height: 50px;
+    color: white;
+}
 
+ul li{
+    display: inline-block;
+    margin-left: 15px;
+}
+
+a{
+    color: white;
+}
+
+.ms_icon_call{
+    margin-right: 15px;
+}
 </style>
